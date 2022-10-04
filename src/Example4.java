42a95730c77a5c0edfb721106829dd3efc8e1564
Bianca Milea <alumil2013@ieselcaminas.org>
2022-10-04 20:36:57 +0200

    example 4

--- src/Example4.java
+++ src/Example4.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Example4 {
+import java.util.Scanner;
+class Example4 {
+    public static void main(String[] argv) {
+
+        float x;
+        float y;
+        Scanner scanner = new Scanner(System.in);
+        x = scanner.nextFloat();
+        y = scanner.nextFloat();
+
+        float suma = x + y;
+        float resta = x - y;
+        float multiplicacion = x * y;
+        float division = x / y;
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multiplicacion);
+        System.out.println(division);
+    }
 }
